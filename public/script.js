4fff7026012071de7f2947975dcb8e225b608b28
rafaellopesvelozo <rafaellopesvelozo@gmail.com.br>
2022-10-07 01:00:40 -0300

    Componization in EJS

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-const closeMessage = document.querySelector("#close-message")
-const message = document.querySelector(".message")
+const closeMessage = document.querySelector("#close-message");
+const message = document.querySelector(".message");
 
-closeMessage.addEventListener("click", ()=>{
-    message.style.display = "none"
-    
-})
-setTimeout(()=>{
-    message.style.display = "none"
-},5000)
+closeMessage.addEventListener("click", () => {
+  message.style.display = "none";
+});
+setTimeout(() => {
+  message.style.display = "none";
+}, 3000);
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,10 @@ const routes = require("express").Router();
 const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
-routes.post("/create", TaskController.createTask)
-routes.get("/getById/:id/:method", TaskController.getById)
-routes.post("/updateOne/:id", TaskController.updateOneTask)
-routes.get("/deleteOne/:id", TaskController.deleteOnetask)
-routes.get("/check/:id", TaskController.taskCheck)
+routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOnetask);
+routes.get("/check/:id", TaskController.taskCheck);
 
 module.exports = routes;
